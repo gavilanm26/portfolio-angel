2a244b22d600146157101bae844a7751ddef83ee
Mauricio Gavilan <mgavilan@fgs.co>
2025-02-10 16:57:21 -0500

    feat: Update contact form and portfolio box layout for improved user experience

--- src/app/components/contact.tsx
+++ src/app/components/contact.tsx
@@ -4,14 +4,14 @@ import Image from "next/image";
 
 const ContactForm = () => {
     return (
-        <div className="bg-darkBg py-6 px-4 sm:px-6 lg:px-8">
+        <div className="py-6 px-4 sm:px-6 lg:px-8">
             <div className="max-w-5xl mx-auto text-center">
                 <h2 className="text-2xl sm:text-3xl font-bold text-secondary mb-4">Contáctame</h2>
                 <p className="text-gray-300 mb-4">
                     Si tienes alguna pregunta, no dudes en ponerte en contacto conmigo. Estoy aquí para ayudarte con tus proyectos tecnológicos.
                 </p>
                 <div className="flex flex-col items-center sm:flex-row sm:justify-center sm:gap-6">
-                    {/* Botón de WhatsApp */}
+
                     <a
                         href="https://wa.me/573214550322?text=Hola, estoy interesado en tus servicios. ¿Podemos hablar?"
                         target="_blank"
--- src/app/components/portfolio-box.tsx
+++ src/app/components/portfolio-box.tsx
@@ -23,24 +23,29 @@ const PortfolioBox = (props: PortfolioBoxProps) => {
     return (
         <div
             key={id}
-            className="p-4 border border-teal-50 rounded-xl flex flex-col justify-between h-[400px]"
+            className="p-4 border border-teal-50 rounded-xl flex flex-col justify-between h-full min-h-[450px]"
         >
-            <div>
-                <h3 className="mb-4 text-xl text-center">{title}</h3>
-                <div className="w-full h-[260px] overflow-hidden rounded-2xl">
+            <div className="flex-1">
+                <div className="h-[100px] flex items-center justify-center text-center">
+                    <h3 className="text-xl">{title}</h3>
+                </div>
+
+                <div className="w-full h-[220px] overflow-hidden rounded-2xl flex justify-center items-center">
                     <Image
                         src={image}
                         alt={title}
                         width={300}
-                        height={200}
+                        height={220}
                         className="w-full h-full object-cover rounded-2xl"
                     />
                 </div>
+
             </div>
 
-            <div className="flex justify-center mt-auto">
+            {/* Asegurar que el botón siempre esté alineado en la parte inferior */}
+            <div className="mt-auto flex justify-center">
                 <button
-                    className="px-4 py-2 transition duration-150 rounded-lg bg-secondary hover:bg-secondary/80 text-white"
+                    className="px-6 py-2 text-white bg-secondary hover:bg-secondary/80 transition duration-150 rounded-lg text-center"
                     onClick={() => setIsModalOpen(true)}
                 >
                     Ver detalles
@@ -49,14 +54,10 @@ const PortfolioBox = (props: PortfolioBoxProps) => {
 
             <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} title={title}>
                 <p>{description}</p>
-                <a
-                    href={urlDemo}
-                    target="_blank"
-                    className="block text-blue-500 underline mt-2"
-                >
-                </a>
+                <a href={urlDemo} target="_blank" className="block text-blue-500 underline mt-2"></a>
             </Modal>
         </div>
+
     );
 };
 
